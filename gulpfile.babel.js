832df47cb9c0f2a6d5e19ca646d374be1692acb0
Zack Hall <zackhall@outlook.com>
2016-05-26 10:27:05 -0700

    Add 'serve' gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,4 @@
+import browserSync from 'browser-sync';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
@@ -5,14 +6,30 @@ import eslint from 'gulp-eslint';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 
+const browserSyncInstance = browserSync.create();
+
+gulp.task('serve', ['js', 'sass', 'copy'], () => {
+  browserSyncInstance.init({
+    server: {
+      baseDir: './dist/',
+    },
+  });
+
+  gulp.watch('app/css/**/*.scss', ['sass']);
+  gulp.watch('app/**/*.html', ['copy-watch']);
+  gulp.watch('app/**/*.js', ['js-watch']);
+});
+
 // Copy all static files to dist/
 gulp.task('copy', () => (
   gulp.src(['app/**/*', '!app/**/*.js', '!app/**/*.scss'])
     .pipe(gulp.dest('dist'))
 ));
 
+gulp.task('copy-watch', ['copy'], browserSyncInstance.reload);
+
 // Use babel to compile ES2015 JS files
-gulp.task('js', () => (
+gulp.task('js', ['lint'], () => (
   gulp.src('app/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -20,6 +37,8 @@ gulp.task('js', () => (
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/js'))
 ));
+
+gulp.task('js-watch', ['js'], browserSyncInstance.reload);
 
 // Run linting on all JS files
 gulp.task('lint', () => (
@@ -36,8 +55,9 @@ gulp.task('sass', () => (
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSyncInstance.stream())
 ));
 
-gulp.task('default', ['lint', 'sass', 'js', 'copy'], () => {
+gulp.task('default', ['js', 'sass', 'copy'], () => {
 
 });
