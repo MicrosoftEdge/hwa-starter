052728e87b391721f2d6f9e21ac72ba7a5cbeeb2
John-David Dalton <john.david.dalton@gmail.com>
2016-06-17 16:41:02 -0700

    Add build:prod script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-var path = require('path');
 var BrowserSync = require('browser-sync-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
 
-const projectRoot = path.resolve(__dirname, 'app');
+var projectRoot = path.resolve(__dirname, 'app');
 
 var config = {
   entry: path.resolve(__dirname, 'app/index.js'),
@@ -55,5 +56,12 @@ var config = {
     ]
   }
 };
+
+if (process.env.NODE_ENV == 'production') {
+  config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin,
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+  );
+}
 
 module.exports = config;
