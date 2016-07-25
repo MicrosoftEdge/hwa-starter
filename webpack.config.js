0f8bf669acacd43a5d21be1e5a05ebbe2f13f889
John-David Dalton <john.david.dalton@gmail.com>
2016-07-25 16:31:05 -0700

    Remove Node v4 requirement.

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,61 @@
-import BrowserSync from 'browser-sync-webpack-plugin';
-import path from 'path';
-import webpack from 'webpack';
+var BrowserSync = require('browser-sync-webpack-plugin'),
+    path = require('path'),
+    webpack = require('webpack');
 
-const projectRoot = path.resolve(__dirname, 'app');
+var projectRoot = path.resolve(__dirname, 'app');
 
-export const entry = path.resolve(__dirname, 'app/index.js');
-
-export const output = {
-  path: path.resolve(__dirname, 'dist/'),
-  filename: 'app.js'
-};
-
-export const plugins = [
-  new BrowserSync({
-    host: 'localhost',
-    port: 3000,
-    server: { baseDir: ['dist'] },
-  }),
-];
-
-export const devtool = '#inline-source-map';
-
-export const module = {
-  preLoaders: [{
-    test: /\.js$/,
-    loader: 'eslint',
-    include: projectRoot,
-    exclude: /node_modules/
-  }],
-  loaders: [{
-    test: /\.js$/,
-    loader: 'babel',
-    include: projectRoot,
-    exclude: /node_modules/
+module.exports = {
+  devtool: '#inline-source-map',
+  entry: path.resolve(__dirname, 'app/index.js'),
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist/')
   },
-  {
-    test: /\.(html|webmanifest|te?xt)$/,
-    loader: 'file-loader?name=[name].[ext]'
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: 'eslint',
+      include: projectRoot,
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      include: projectRoot,
+      exclude: /node_modules/
+    },
+    {
+      test: /\.(html|webmanifest|te?xt)$/,
+      loader: 'file-loader?name=[name].[ext]'
+    },
+    {
+      test: /\.(s?css)$/,
+      loaders: ['style', 'css', 'sass']
+    },
+    {
+      test: /\.(png|jpe?g|gif|ico)$/,
+      loader: 'file-loader?name=[path][name].[ext]&context=./app'
+    },
+    {
+      test: /\.svg$/,
+      loaders: ['url-loader', 'svg-loader']
+    },
+    {
+      test: /\.(?:eot|ttf|woff)$/,
+      loaders: ['url-loader']
+    }]
   },
-  {
-    test: /\.(s?css)$/,
-    loaders: ['style', 'css', 'sass']
-  },
-  {
-    test: /\.(png|jpe?g|gif|ico)$/,
-    loader: 'file-loader?name=[path][name].[ext]&context=./app'
-  },
-  {
-    test: /\.svg$/,
-    loaders: ['url-loader', 'svg-loader']
-  },
-  {
-    test: /\.(?:eot|ttf|woff)$/,
-    loaders: ['url-loader']
-  }]
+  plugins: [
+    new BrowserSync({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] },
+    })
+  ]
 };
 
 if (process.env.NODE_ENV == 'production') {
-  plugins.push(
+  exports.plugins.push(
     new webpack.optimize.OccurrenceOrderPlugin,
     new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
   );
